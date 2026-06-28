8c4e4491ba96e97af18a8e9a548676489b5037f6
Chad Estioco <chadestioco@gmail.com>
2026-06-28 15:07:24 +0800

    Update ytdlp version.

--- ytdlpl.py
+++ ytdlpl.py
@@ -11,6 +11,8 @@ LIST_NAME = "YTDLPLQ"
 DL_DEST = "/root/Videos/ytdlpl"
 
 HTTP_STATUS_ERROR_REGEX = re.compile(r".*HTTP\s+Error\s+(\d+).*")
+MAX_FAIL_COUNT = 10
+FAIL_COUNTS = {}
 
 logger = logging.getLogger("ytdl")
 logger.setLevel(logging.INFO)
@@ -29,13 +31,21 @@ def parse_http_error(error_msg):
 if __name__ == "__main__":
     redis_client = redis.Redis(host="redis", port=6379, db=0)
     ytdl_opts = {
-        "outtmpl": DL_DEST + "/%(title)s.%(ext)s"
+        "outtmpl": DL_DEST + "/%(title)s.%(ext)s",
+        "writesubtitles": True,
+        "subtitleslangs": ["en"],
+        "extractor_args": {
+            "generic": {
+                "impersonate": [""]
+            }
+        }
     }
     
     while True:
         logger.info("Waiting for an URL...")
         url = redis_client.blpop(LIST_NAME)
         url = url[1].decode("utf-8")
+        FAIL_COUNTS[url] = 0
         logger.info("Got %s, downloading..." % url)
         with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
             try:
@@ -45,16 +55,28 @@ if __name__ == "__main__":
 
                 if httpe.code in (503, 504):
                     logger.info("code is retriable, re-enqueueing...")
-                    redis_client.rpush(LIST_NAME, url)
+                    FAIL_COUNTS[url] += 1
+                    if FAIL_COUNTS[url] < MAX_FAIL_COUNT:
+                        redis_client.rpush(LIST_NAME, url)
+                    else:
+                        logger.error("%s has been retried too many times, retiring..." % url)
                 else:
                     logger.info("%s can't be retried." % url)
             except socket.gaierror as GAIError:
-                logger.exception("Socker error occurred. Probably retriable...")
-                redis_client.rpush(LIST_NAME, url)
+                logger.exception("Socket error occurred. Probably retriable...")
+                FAIL_COUNTS[url] += 1
+                if FAIL_COUNTS[url] < MAX_FAIL_COUNT:
+                    redis_client.rpush(LIST_NAME, url)
+                else:
+                    logger.error("%s has been retried too many times, retiring..." % url)
             except youtube_dl.utils.DownloadError as DLError:
                 http_status = parse_http_error(sys.exc_info()[1].msg)
                 if http_status is not None and 400 <= http_status <= 499:
                     logger.error("Fetching video data failed with client error: %s. Will NOT retry." % http_status)
                 else:
                     logger.exception("Super generic error. Will attempt retry...")
-                    redis_client.rpush(LIST_NAME, url)
+                    FAIL_COUNTS[url] += 1
+                    if FAIL_COUNTS[url] < MAX_FAIL_COUNT:
+                        redis_client.rpush(LIST_NAME, url)
+                    else:
+                        logger.error("%s has been retried too many times, retiring..." % url)
